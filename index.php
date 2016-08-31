35baeada530957039c3d6be49603207a4ee4cac5
ihaveamac <ian@ianburgwin.net>
2016-08-31 15:11:42 -0700

    add link to github

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include_once("_header.php");
 <div class="container">
   <h1>Nintendo 3DS hacking helper</h1>
   <p>Something goes here!</p>
-  <a class="btn btn-primary" href="model.php" role="button">Start hax</a>
+  <p><a class="btn btn-primary" href="model.php" role="button">Start hax</a></p>
+  <p><a href="https://github.com/ihaveamac/ez3ds">Source on GitHub</a></p>
 </div>
 <?php include_once("_footer.php"); ?>
